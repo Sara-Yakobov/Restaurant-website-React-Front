8b82039691ab1bdb58651075cac8e267dd078653
sushi <sushi@email.com>
2023-06-17 22:54:46 +0300

    עובד

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from "react";
+import { Button } from "@mui/material";
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import axios from "axios";
+import Menu from "./Menu";
+import picture from "./image/KARELA-09140.jpg";
 import './App.css';
 
+
 function App() {
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    axios.get('https://apiproject3.braveocean-96b61207.swedencentral.azurecontainerapps.io/categories').then(response => {
+      setCategories(response.data);
+    });
+  }, []);
+
+  const handleClick = () => {
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home handleClick={handleClick} />} />
+          <Route path="/category/:id" element={<Menu categories={categories}/>}/>
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+function Home({ handleClick }) {
+  return (
+    <div className="main">
+      <div className="text">
+        <h1>ברוכים הבאים למסעדת קפה קפה</h1>
+        <Button variant="contained" color="primary" sx={{ height: 60, width: 200 }} onClick={handleClick}>
+          <Link style={{ fontSize: "1.5rem", fontFamily: "Arial, sans-serif" }} to="/category/:id">לתפריט לחץ כאן</Link>
+        </Button>
+      </div>
+      <img src={picture} alt="תמונה" className="image" />
     </div>
   );
 }
---
+++ src/Menu.js
@@ -0,0 +1,105 @@
+import React from 'react';
+import { AppBar, Toolbar, Typography, Button, CardMedia, Card, CardContent } from '@mui/material';
+import axios from 'axios';
+import { useParams, Link } from 'react-router-dom';
+import picture from './image/KARELA-09140.jpg';
+
+function Menu({ categories }) {
+  const params = useParams();
+  const [dishes, setDishes] = React.useState([]);
+
+  React.useEffect(() => {
+    if (params.id === undefined) {
+      fetchDishes();
+    } else {
+      filterByCategory(params.id);
+    }
+  }, [params.id]);
+
+  function fetchDishes() {
+    axios.get(`https://apiproject3.braveocean-96b61207.swedencentral.azurecontainerapps.io/dishes`)
+      .then(response => {
+        setDishes(response.data);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }
+
+  function filterByCategory(category_id) {
+    axios.get(`https://apiproject3.braveocean-96b61207.swedencentral.azurecontainerapps.io/dishes?category_id=${category_id}`)
+      .then(response => {
+        setDishes(response.data);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }
+
+  return (
+    <div
+      style={{
+        backgroundImage: `url(${picture})`,
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        minHeight: '100vh',
+      }}
+    >
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            התפריט שלנו
+          </Typography>
+          <div>
+            {categories.map((category) => (
+              <Button
+                key={category.id}
+                variant="contained"
+                color="primary"
+                size="large"
+                component={Link}
+                to={`/category/${category.id}`}
+                sx={{ mx: 2, textTransform: "none", fontSize: "1.5rem" }}
+              >
+                {category.name}
+              </Button>
+            ))}
+          </div>
+        </Toolbar>
+      </AppBar>
+      <div style={{ display: 'flex', flexWrap: 'wrap' }}>
+        {dishes.map((dish) => (
+          <Card key={dish.id} sx={{ width: 400, margin: '10px', padding: '20px', backgroundColor: 'white' }}>
+            <CardMedia
+              component="img"
+              height="300"
+              image={dish.imageUrl}
+              alt={dish.name}
+            />
+            <CardContent>
+              <Typography variant="h6" component="div">
+                {dish.name} - ₪{dish.price}
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                {dish.description}
+              </Typography>
+              {dish.is_gluten_free && (
+                <Typography variant="body2" color="text.secondary">
+                  מכיל גלוטן
+                </Typography>
+              )}
+              {dish.is_vegetarian && (
+                <Typography variant="body2" color="text.secondary">
+                  (המנה צמחונית)
+                </Typography>
+              )}
+            </CardContent>
+          </Card>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Menu;
